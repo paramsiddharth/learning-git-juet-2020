c8fa6058f6ebd3db86cc083ef487c2c6d5204526
Khushi Jain <khushi.jain0704@gmail.com>
2020-11-22 16:13:10 +0530

    updated

--- khushi.c
+++ khushi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char* string = "Hello!"; // Your name here. ^_^
+	char* string = "Khushi here ! Hope you're having a great day !! "; // Your name here. ^_^
 	printf("%s\n", string);
 	return 0;
 }
